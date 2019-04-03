bf8f1fe2ae7ff25a2a3cb6689f9875761422a6b4
Rodrigo Andrade <warodrigoandrade@gmail.com>
2019-04-02 23:35:17 -0300

    Modificado body de requisição]

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const crypto = require('crypto');
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const exec = require('child_process').exe;
+const http = require('http');
 
 /* Segredo utilizado pelo webhook para assinar chamada */
 const segredo = process.env.PIPELINE_SEGREDO;
@@ -19,17 +20,30 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/pipelines/iforgot', function (req, res) {
-    const assinatura = "sha1=" + crypto.createHmac('sha1', segredo).update(req.body.toString()).digest('hex');
-    console.log(req.headers);
-    console.log(assinatura);
-    if (req.headers['x-hub-signature'] == assinatura) {
-        exec('cd ' + repositorio + ' && git pull');
-        res.json({ mensagem: 'Pipeline executado com sucesso!' });
-    }
+// app.post('/pipelines/iforgot', function (req, res) {
+//     const assinatura = "sha1=" + crypto.createHmac('sha1', segredo).update(req.body.toString()).digest('hex');
+//     console.log(req.headers);
+//     console.log(assinatura);
+//     if (req.headers['x-hub-signature'] == assinatura) {
+//         exec('cd ' + repositorio + ' && git pull');
+//         res.json({ mensagem: 'Pipeline executado com sucesso!' });
+//     }
 
-});
+// });
 
-app.listen(8082, function () {
-    console.log('Projeto Pipeline rodando na porta 8082');
-});
+http.createServer(function (req, res) {
+    req.on('data', function (chunk) {
+        console.log(chunk);
+        let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
+
+        if (req.headers['x-hub-signature'] == sig) {
+            exec('cd ' + repo + ' && git pull');
+        }
+    });
+
+    res.end();
+}).listen(8082);
+
+// app.listen(8082, function () {
+//     console.log('Projeto Pipeline rodando na porta 8082');
+// });
