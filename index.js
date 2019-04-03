920e3fb2c1386f9b3d4f3a7296958afbb29e02ec
Rodrigo Andrade <warodrigoandrade@gmail.com>
2019-04-02 23:38:36 -0300

    Modificado body de requisição]

--- index.js
+++ index.js
@@ -16,34 +16,35 @@ const repositorio = "~/iforgot";
 const app = express();
 
 /* Utilizando apenas de segurança. */
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(cors());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-// app.post('/pipelines/iforgot', function (req, res) {
-//     const assinatura = "sha1=" + crypto.createHmac('sha1', segredo).update(req.body.toString()).digest('hex');
-//     console.log(req.headers);
-//     console.log(assinatura);
-//     if (req.headers['x-hub-signature'] == assinatura) {
-//         exec('cd ' + repositorio + ' && git pull');
-//         res.json({ mensagem: 'Pipeline executado com sucesso!' });
-//     }
+app.post('/pipelines/iforgot', function (req, res) {
+    console.log(req.body.toString();
+    const assinatura = "sha1=" + crypto.createHmac('sha1', segredo).update(req.body.toString()).digest('hex');
+    console.log(req.headers);
+    console.log(assinatura);
+    if (req.headers['x-hub-signature'] == assinatura) {
+        exec('cd ' + repositorio + ' && git pull');
+        res.json({ mensagem: 'Pipeline executado com sucesso!' });
+    }
 
-// });
+});
 
-http.createServer(function (req, res) {
-    req.on('data', function (chunk) {
-        console.log(chunk.toString());
-        let sig = "sha1=" + crypto.createHmac('sha1', segredo).update(chunk.toString()).digest('hex');
+// http.createServer(function (req, res) {
+//     req.on('data', function (chunk) {
+//         console.log(chunk.toString());
+//         let sig = "sha1=" + crypto.createHmac('sha1', segredo).update(chunk.toString()).digest('hex');
 
-        if (req.headers['x-hub-signature'] == sig) {
-            exec('cd ' + repo + ' && git pull');
-        }
-    });
+//         if (req.headers['x-hub-signature'] == sig) {
+//             exec('cd ' + repo + ' && git pull');
+//         }
+//     });
 
-    res.end();
-}).listen(8082);
+//     res.end();
+// }).listen(8082);
 
-// app.listen(8082, function () {
-//     console.log('Projeto Pipeline rodando na porta 8082');
-// });
+app.listen(8082, function () {
+    console.log('Projeto Pipeline rodando na porta 8082');
+});
